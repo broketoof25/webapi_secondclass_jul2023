dc7f6fca44ce9427e15db99029b3e30c3e78313a
Andrew Lall <andrew.s.lall@gmail.com>
2023-08-02 16:16:55 -0400

    Initial commit

---
+++ EmployeesHrSolution/EmployeesHrApi/Controllers/EmployeesController.cs
@@ -0,0 +1,71 @@
+﻿
+
+
+
+
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using EmployeesHrApi.Data;
+using EmployeesHrApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesHrApi.Controllers;
+
+public class EmployeesController : ControllerBase
+{
+
+    private readonly EmployeeDataContext _context;
+    private readonly ILogger<EmployeesController> _logger;
+    private readonly IMapper _mapper;
+    private readonly MapperConfiguration _config;
+
+    public EmployeesController(EmployeeDataContext context, ILogger<EmployeesController> logger, IMapper mapper, MapperConfiguration config)
+    {
+        _context = context;
+        _logger = logger;
+        _mapper = mapper;
+        _config = config;
+    }
+
+
+    // GET /employees/3
+    [HttpGet("/employees/{employeeId:int}")]
+    public async Task<ActionResult> GetAnEmployeeAsync(int employeeId)
+    {
+        _logger.LogInformation("Got the following employeeId {0}", employeeId);
+        var employee = await _context.Employees
+            .Where(e => e.Id == employeeId)
+            .ProjectTo<EmployeeDetailsResponseModel>(_config)
+            .SingleOrDefaultAsync();
+
+        if (employee is null)
+        {
+            return NotFound(); // 404
+        }
+        else
+        {
+            return Ok(employee);
+        }
+
+    }
+
+
+    // GET /employees
+    [HttpGet("/employees")]
+    public async Task<ActionResult<EmployeesResponseModel>> GetEmployeesAsync([FromQuery] string department = "All")
+    {
+        var employees = await _context.GetEmployeesByDepartment(department)
+            .ProjectTo<EmployeesSummaryResponseModel>(_config)
+            .ToListAsync(); // runs the query
+
+        var response = new EmployeesResponseModel
+        {
+            Employees = employees,
+            ShowingDepartment = department
+        };
+        return Ok(response);
+    }
+
+
+}
---
+++ EmployeesHrSolution/EmployeesHrApi/Models/EmployeeModels.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace EmployeesHrApi.Models;
+
+public record EmployeesResponseModel
+{
+    public List<EmployeesSummaryResponseModel> Employees { get; set; } = new();
+    public string ShowingDepartment { get; set; } = string.Empty;
+}
+
+public record EmployeesSummaryResponseModel
+{
+    public string Id { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Department { get; set; } = string.Empty;
+}
+
+// GET /employees/{id}
+public record EmployeeDetailsResponseModel 
+{
+    public string Id { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Department { get; set; } = string.Empty;
+    public string PhoneExtension { get; set; } = string.Empty;
+}
